5b7cb507cf2cd04e7bc35779a6392ea58bf611a6
unknown <nicolas.s.fernandes@unesp.br>
2026-03-23 17:38:11 -0300

    first commit

---
+++ src/test/java/Algx/GerenciamentoDeCarros/GerenciamentoDeCarrosApplicationTests.java
@@ -0,0 +1,13 @@
+package Algx.GerenciamentoDeCarros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeCarrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
